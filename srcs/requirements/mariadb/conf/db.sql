997176f53c91500cf75be6f078e2ab5be16ef5b4
Saad Akllam <sakllam@e2r3p13.1337.ma>
2022-11-03 19:49:16 +0100

    adding vars and trying to redirect IP is the task

--- srcs/requirements/mariadb/conf/db.sql
+++ srcs/requirements/mariadb/conf/db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE if not exists my_database;
-CREATE USER 'EVBLOOD'@'%';
-GRANT ALL PRIVILEGES ON my_database.* to 'EVBLOOD'@'%' IDENTIFIED BY '123';
-alter user 'root'@'localhost' identified by '123';
+CREATE USER $MYSQL_USER@'%';
+GRANT ALL PRIVILEGES ON my_database.* to $MYSQL_USER@'%' IDENTIFIED BY $MYSQL_PASSWORD;
+alter user 'root'@'localhost' identified by $MYSQL_ROOT_PASSWORD;
 FLUSH PRIVILEGES;
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -8,12 +8,12 @@ mv wp-cli.phar /usr/local/bin/wp
 mkdir -p /var/www/html
 sed -i -e 's/listen =.*/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 wp core download --path="/var/www/html"  --allow-root
-wp config create --dbname=my_database --dbuser=EVBLOOD --dbpass=123 --dbhost=mariadb --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_host --path=/var/www/html --allow-root --skip-check --extra-php <<PHP
 define('WP_REDIS_PORT', 6379);
 define('WP_REDIS_HOST', 'redis');
 define('WP_REDIS_DISABLED', false);
 PHP
-wp core install --url=https://localhost --title=inception --admin_user=EVBLOOD --admin_password=123 --admin_email=my@email.com --allow-root --path=/var/www/html
+wp core install --url=$url --title=$title --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email --allow-root --path=/var/www/html
 wp plugin install redis-cache --path=/var/www/html --allow-root
 wp plugin activate redis-cache --path=/var/www/html --allow-root
 wp redis enable --path=/var/www/html --allow-root
