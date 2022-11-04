dea2fe0358402da8b687928dc376bb3a96bc78c4
Saad Akllam <sakllam@e2r3p13.1337.ma>
2022-11-04 21:04:34 +0100

    depend on and stuf

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -13,6 +13,7 @@ define('WP_REDIS_PORT', 6379);
 define('WP_REDIS_HOST', 'redis');
 define('WP_REDIS_DISABLED', false);
 PHP
+echo "start " $MYSQL_DATABASE $MYSQL_USER $MYSQL_PASSWORD $MYSQL_host "   end"
 wp core install --url=$url --title=$title --admin_user=$admin_user --admin_password=$admin_password --admin_email=$admin_email --allow-root --path=/var/www/html
 wp plugin install redis-cache --path=/var/www/html --allow-root
 wp plugin activate redis-cache --path=/var/www/html --allow-root
