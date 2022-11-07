bd6cd810879769f21995914ddb127a4a1f79f4eb
Saad Akllam <sakllam@e2r2p15.1337.ma>
2022-11-07 15:00:53 +0100

    adding vars to ftp

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -1,11 +1,11 @@
 apt update -y;
 apt install vsftpd -y
 service vsftpd start;
-adduser evblood;
-echo "evblood:123456" | chpasswd;
-mkdir -p /home/evblood/ftp/;
-chown -R evblood:evblood /home/evblood/ftp/;
-echo "evblood" >> /etc/vsftpd.userlist
+adduser $username;
+echo "$username:$userpasswrd" | chpasswd;
+mkdir -p /home/$username/ftp/;
+chown -R $username:$username /home/$username/ftp/;
+echo $username >> /etc/vsftpd.userlist
 echo "local_enable=YES" >> /etc/vsftpd.conf;
 echo "write_enable=YES" >> /etc/vsftpd.conf;
 echo "chroot_local_user=YES" >> /etc/vsftpd.conf;
@@ -16,5 +16,5 @@ echo "pasv_max_port=40005" >> /etc/vsftpd.conf;
 echo "userlist_enable=YES" >> /etc/vsftpd.conf;
 echo "userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf;
 echo "userlist_deny=NO" >> /etc/vsftpd.conf;
-echo "secure_chroot_dir=/home/evblood/ftp" >> /etc/vsftpd.conf;
+echo "secure_chroot_dir=/home/$username/ftp" >> /etc/vsftpd.conf;
 /usr/sbin/vsftpd
